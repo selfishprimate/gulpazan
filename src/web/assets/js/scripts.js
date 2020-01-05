dc34b561b00b93c9d222dbfbea9acbb73e313b4b
Halil İbrahim Çakıroğlu <ibrahimchll@hotmail.com>
2020-01-05 12:56:40 +0300

    Bugfixes.

--- src/web/assets/js/scripts.js
+++ src/web/assets/js/scripts.js
@@ -3,7 +3,7 @@ const gulpazan = "Use the power of ES6 with no worry!"
 console.log(gulpazan);
 
 hello = () => {
-    return "Hello World!";
+    return console.log("Hello World!");
 }
 
-let mf = 4;
+hello();
