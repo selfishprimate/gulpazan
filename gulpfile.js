ae7e6f16b56a5eaa98132d2329781838c1941de2
Halil İbrahim Çakıroğlu <ibrahimchll@hotmail.com>
2020-01-08 16:11:10 +0300

    Express.js integration continues.

--- gulpfile.js
+++ gulpfile.js
@@ -12,13 +12,13 @@ function nunjucks(done) {
     .pipe(nunjucksRender({
       path: ["src/templates"]
     }))
-    .pipe(dest("src/web"))
+    .pipe(dest("src/public"))
   done()
 }
 
 // Sass compiler
 function sassify(done) {
-  return src('src/web/assets/sass/**/*.scss')
+  return src('src/public/assets/sass/**/*.scss')
     .pipe(sourcemaps.init())
     // gulp-sass kullanarak Sass dosyasını CSS'e çeviriyor. (nested, compact, expanded, compressed)
     .pipe(sass({ outputStyle: 'expanded' })).on('error', function swallowError(error) {
@@ -26,7 +26,7 @@ function sassify(done) {
       this.emit('end');
     })
     .pipe(sourcemaps.write())
-    .pipe(dest('src/web/assets/css'))
+    .pipe(dest('src/public/assets/css'))
     .pipe(browserSync.reload({ stream: true }))
   done()
 }
@@ -34,8 +34,8 @@ function sassify(done) {
 function browser_sync(done) {
   browserSync.init({
     watch: true,
-    server: { baseDir: 'src/web' },
-    port: 8080
+    server: { baseDir: 'src/public' },
+    port: 5000
   })
   done()
 }
@@ -43,28 +43,28 @@ function browser_sync(done) {
 // Moves all the Bootstrap related JavaScript files into the js folder
 function bootstrapper(done) {
   return src(['node_modules/bootstrap/dist/js/bootstrap.js', 'node_modules/jquery/dist/jquery.js', 'node_modules/popper.js/dist/popper.js'])
-    .pipe(dest('src/web/assets/js'))
+    .pipe(dest('src/public/assets/js'))
     .pipe(browserSync.stream())
   done()
 }
 
 function html() {
-  return src('src/web/*.html')
+  return src('src/public/*.html')
     .pipe(dest('dist'))
 }
 
 function images() {
-  return src('src/web/assets/images/**/*.+(png|jpg|gif|svg)')
+  return src('src/public/assets/images/**/*.+(png|jpg|gif|svg)')
     .pipe(dest('dist/assets/images'))
 }
 
 function fonts() {
-  return src('src/web/assets/fonts/**/*')
+  return src('src/public/assets/fonts/**/*')
     .pipe(dest('dist/assets/fonts'))
 }
 
 function css() {
-  return src('src/web/assets/css/**/*.css')
+  return src('src/public/assets/css/**/*.css')
     .pipe(dest('dist/assets/css/'))
 }
 
@@ -72,7 +72,7 @@ function js() {
   // The ordering is very important here!
   // First it moves all the js files
   // Second it overwrites the "scripts.js" file with the transpiled one
-  return src('src/web/assets/js/**/*.js')
+  return src('src/public/assets/js/**/*.js')
     .pipe(dest('dist/assets/js/'))
 }
 
